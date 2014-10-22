e5d5e161b6ca5a097988e5cd4179b2bcfa6c33fb
Steven Li <listeven@usc.edu>
2014-10-21 17:42:27 -0700

    my things

---
+++ QuietKissing_PROJECT/Assets/Scripts/NetworkMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMovement : Photon.MonoBehaviour {
+
+    private Vector3 correctPlayerPos;
+    private Quaternion correctPlayerRot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!photonView.isMine)
+        {
+            transform.position = Vector3.Lerp(transform.position,correctPlayerPos,Time.deltaTime*5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
+        }
+	}
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            correctPlayerPos = (Vector3)stream.ReceiveNext();
+            correctPlayerRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+}
---
+++ QuietKissing_PROJECT/Assets/Scripts/NetworkingInitialization.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkingInitialization : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(".1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        GameObject monster = PhotonNetwork.Instantiate("Prefabs/BlueDragon", Vector3.zero, Quaternion.identity, 0);
+        CharacterController controller = monster.GetComponent<CharacterController>();
+        controller.enabled = true;
+        Camera cam = monster.GetComponentInChildren<Camera>();
+        cam.enabled = true;
+        FPSInputController input = monster.GetComponent<FPSInputController>();
+        input.enabled = true;
+        MouseLook look = monster.GetComponent<MouseLook>();
+        look.enabled = true;
+        AudioListener listener = monster.GetComponentInChildren<AudioListener>();
+        listener.enabled = true;
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
