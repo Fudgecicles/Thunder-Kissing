212a05791f762bc14c8c5c3dd88dd83c4fbdee21
Steven Li <listeven@usc.edu>
2014-11-05 04:59:42 -0800

    stuff idk this might break shit

---
+++ QuietKissing_PROJECT/Assets/_Scripts/DoorOpener.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : Photon.MonoBehaviour {
+
+    bool canOpen = false;
+    bool open = false;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (canOpen)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                photonView.RPC("openRPC", PhotonTargets.AllBuffered);
+            }
+        }
+	}
+
+    [RPC]
+    public void openRPC()
+    {
+        if (!open)
+        {
+            rigidbody.AddForce(-transform.forward * 300);
+            open = true;
+
+        }
+        else
+        {
+            rigidbody.AddForce(transform.forward * 300);
+            open = false;
+        }
+        StartCoroutine(lockPos());
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            canOpen = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            canOpen = false;
+        }
+    }
+
+    IEnumerator lockPos()
+    {
+        yield return new WaitForSeconds(.1f);
+        rigidbody.velocity = Vector3.zero;
+    }
+
+
+}
